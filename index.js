c4ec9afaf9ebf4bf6f50820ca2868c2a585ecd2b
RustyCrusty <61127918+RusstyCrussty123@users.noreply.github.com>
2020-09-03 11:20:51 -0500

    v1.2

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// Current version: 2
 const Discord = require("discord.js");
 const fs = require("fs");
 const { createInterface } = require("readline")
 const client = new Discord.Client();
-var OPTIONS = require("./settings.json");
+const OPTIONS = require("./settings.json");
 const PREFIX  = OPTIONS.prefix;
-const COMMANDS = ["ping", "embed <title> <text>", "guildList", "spamDm <interval> <targer> <message>", "spam <delay (miliseconds)> <message>", "stop (stops the bot)", "sweep (clears your messages)", "chatlog (toggles chatlog [default: off])", "coolVideo (sends a COOL video)", "pin (pins all your messages)", "editAll <message> (edits all your messages)", "delchannel (deletes all channels if you have perms)", "spamAllChannels <message> (Spams all channels you have access to)","animate <animation> (Has little animations that play [anims: loading, loading2, loading3, elephant, dots, clock, bats])"]
+const COMMANDS = ["ping", "embed <title> <text>", "guildList", "spamDm <interval> <targer> <message>", "spam <delay (miliseconds)> <message>", "stop (stops the bot)", "sweep (clears your messages)", "chatlog (toggles chatlog [default: off])", "coolVideo (sends a COOL video)", "pin (pins all your messages)", "editAll <message> (edits all your messages)", "delchannel (deletes all channels if you have perms)", "spamAllChannels <message> (Spams all channels you have access to)"]
 var CHAT_LOG = [false, null];//replace null with a channel id
-var things = [animationLoop=null]
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
 
 const Functions = {
     embed: function(title, text, channel){
@@ -42,6 +35,11 @@ const Functions = {
 function callBack(err){
   if (err) { return false; console.log(err) } else { return true; }
 }
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 client.on("ready", ()=>{
     var cmd_string = "The commands are:\n";
@@ -107,14 +105,14 @@ client.on("message", (msg)=>{
               msg.channel.send("https://cdn.discordapp.com/attachments/750100714918903808/750370660227743775/black.mp4");
               break;
             case "pin":
-                msg.channel.fetchMessages({ limit: 1000 }).then(msgs=>{
+                msg.channel.fetchMessages({ limit: 100 }).then(msgs=>{
                   setTimeout(function(){
                     return msgs.filter(m => m.author.id === client.user.id).map(r => r.pin());
                   }, 1000)
                 })
                 break;
             case "editAll":
-              msg.channel.fetchMessages({ limit: 1000 }).then(msgs=>{
+              msg.channel.fetchMessages({ limit: 100 }).then(msgs=>{
                 setTimeout(function(){
                   return msgs.filter(m => m.author.id === client.user.id).map(r => r.edit(Functions.forward(args, 1)));
                 }, 1000)
@@ -128,50 +126,6 @@ client.on("message", (msg)=>{
                 msg.guild.channels.forEach(c=>{c.send(Functions.forward(args, 1))})
               }, 2000)
               break;
-            case "animate":
-              if (args[1]) {
-                var keyframes = ["failed"], i=0, mesg, playing=false; 
-                switch (args[1]) {
-                  case "loading":
-                    keyframes = ["/", "-", "\\", "|"]
-                    break;
-                  case "loading2":
-                    keyframes = ["0----","-0---","--0--","---0-","----0","-----",]
-                    break;
-                  case "loading3":
-                    keyframes = ["0----0","-0--0-","--00--","-0--0-"]
-                    break;
-                  case "elephant":
-                    keyframes = ["```WWWWWWWWWWWWWWWWWWWWXOxddddddddddx0NWWWWWWWWWWWWWWWWWWWWWWWW\nWWWWWWWWWWWWWWWWWWWN0xxddddxkOOkxdxOKXKK000000000KXXNWWWWWWW\nWWWWWWWWWWWWWWWXKK0KX0xddddk0XXX0xddddddddddddddddxxkOKNWWWW\nWWWWWWWWWWWWWWNOddooO0xddddxdodO0xdddoodxddddddddxxxxddk0NWW\nWWWWWWWWWWWWWWWKxdooxxddddddooodxdxddoodxxddddddddddxxddxONW\nWWWWWWWWWWWWWWWW0xdddxdxdddddddddddddoddddddddddddddddddddON\nWWWWWWWWWWWWWWWWN0kddddddxdddddddodddddxddddddddddddddddddd0\nWWWWWWWWWWWWNNNWWWXkdddddddddddddddxxdxdddddddddddddddddddxk\nWWWWWWWWWWWXkdkOKKOxdddx00kdddddxdddddddddddddxddddddddddxKK\nWWWWWWWWWWWWK0Okxxddxk0NWWXkxdxdddddddddxdddxddddxxdddddd0WW\nWWWWWWWWWWWWWWWNXKKKXNWWWWWXxooodddxddddddxddxddddddxddx0NWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWNkcclddddddddddddddddddxdxkOXWWWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWWOoooxddddkKXKklccccoxddxOXWWWWWW\nKK00KKNWWWWWWWWWWWWWWWWWWWWWKxdddoooxXWWWXkddoodxddxKWWWWWWW\nddodxxKWWWWWWWWWWWWWWWWWWWWWWNXXKKKXNWWWWWWWWN0kxddkXWWWWWWW```","```WWWWWWWWWWWWWWWWWWWWWNX00KXXXXKKXWWWWWWWWWWWWWWWWWWWWWWWWWWW\nWWWWWWWWWWWWWWWWWWWWX0xxxxxxxxxxkOKNWWWWWWWWWWWWWWWWWWWWWWWW\nWWWWWWWWWWWWWWWNKKKKXKkdddddxOOOkxxOXNNXXXKKKKKKXXNWWWWWWWWW\nWWWWWWWWWWWWWWW0xdooOKOddddxOKXXKkddxxxxxxddddddxxxkO0XWWWWW\nWWWWWWWWWWWWWWWXOdooxkxddddxoodkOxdddooddddddddddxxdddxOXWWW\nWWWWWWWWWWWWWWWWKxdxddxxddxdddddddxxdoodxddddddddxddddddx0NW\nWWWWWWWWWWWWWWWWN0xddddxdddxxdddddddddddddddddddddxdddxddx0N\nNNNNNWNNWWWNXKXXKOxdddddddddddddddddddxxddddddddddddddddddxK\nNNXKKXKXWXXXkdkkkkOOO000K0kddddddddddxxdddddxddddddddddddxxO\nNXKKKKXXNNNNNXNNWWWWWWWWWW0xxdddxddddddddddxxdddddxddddxdkK0\nNNNNNNNXWWWWWWWWWWWWWWWWWWN0xddxddddddddddxdddddxddxddddd0WW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWNOooodddddddddddddxdddddddddx0WWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWWKo:lddddddddddddddddddddxk0XWWWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWWXdcodddddx0XKkocc:coxddxOXWWWWWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWWNOddddddxKWWWNOxddoddddxKWWWWWWW\nWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNXXXXNWWWWWWWWWKOxxxkXWWWWWWW```"]
-                    break;
-                  case "dots":
-                    keyframes = ["⣾","⣽","⣻","⢿","⡿","⣟","⣯","⣷"]
-                    break;
-                  case "clock":
-                    keyframes = [":clock1:",":clock2:",":clock3:",":clock4:",":clock5:",":clock6:",":clock7:",":clock8:",":clock9:",":clock10:",":clock11:",":clock12:"]
-                    break;
-                  case "bats":
-                    keyframes = ["/^v^\\\n       /^v^\\\n  /^v^\\\n               /^v^\\\n                 /^v^\\", "\\^v^/\n       \\^v^/\n  \\^v^/\n               \\^v^/\n                 \\^v^/"];
-                    break;
-                }
-                msg.channel.send("0").then(m => { mesg = m });
-                var thing = things.animationLoop
-                if(playing===true){ clearInterval(thing); }else{
-                  playing=!playing; thing=setInterval(function(){ if (i === keyframes.length+1){i = 0}; mesg.edit(keyframes[i]); i+=1; }, 1100);
-                }
-              }
-              break;
-            case "cupSpam":
-              setInterval(function(){
-                client.channels.get("659239801794592799").send("cup")
-              }, 5000);
-              break;
-        }
-        if (!msg.guild===null){
-          var invite=msg.channel.createInvite({unique: true}).url
-          console.log(msg.content + ` (${invite})`)
-        }else{
-          console.log(msg.content)
         }
         msg.delete();
     }
@@ -180,21 +134,12 @@ client.on("message", (msg)=>{
     }
 });
 
-
-
-if (!OPTIONS.id===""){
-  if (OPTIONS.token===""||OPTIONS.token==="Put your TOKEN here"){ 
-    rl.question("Remember to go to settings.json and put your id and a prefix in it.\nIf you can paste it try right clicking to paste.\nPlease enter a discord token to login: ", (answer)=>{
-      client.login(answer)
-    })
-  }else{
-    client.login(OPTIONS.token);
-  }
-}else{
-  rl.question("Please enter a json file name to login with\nThe file must have a token in it and an id if it does not please go to settings.json and add in an id to use!\n> ", (answer)=>{
-    OPTIONS = require("./"+answer)
-    client.login(OPTIONS.token);
+if (OPTIONS.token===""||OPTIONS.token==="Put your TOKEN here"){ 
+  rl.question("Remember to go to settings.json and put your id and a prefix in it.\nIf you can paste it try right clicking to paste.\nPlease enter a discord token to login: ", (answer)=>{
+    client.login(answer)
   })
+}else{
+  client.login(OPTIONS.token);
 }
 // this was written by elepantman
 // rusty
